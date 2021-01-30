6c65bad4b4c089f60da438f13da241e6e7a9e950
lfvperes <luisfvperes@gmail.com>
2021-01-30 05:26:29 -0300

    aula 3 completa

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget/'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizLogo from '../src/components/QuizLogo'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizContainer from '../src/components/QuizContainer';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // // criando component para o background
 // const BackgroundImage = styled.div`
@@ -15,29 +21,40 @@ import QuizLogo from '../src/components/QuizLogo'
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz sobre felinos</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
-        <Widget.Header>
-          <h1>CSS is Awesome!</h1>
-        </Widget.Header>
+          <Widget.Header>
+            <h1>CSS is Awesome!</h1>
+          </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum</p>
+            <form onSubmit={function(eventInfo) {
+              eventInfo.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do React');
+            }}>
+              <Input
+                onChange={(eventInfo) => {
+                  setName(eventInfo.target.value);
+                }}
+                placeholder="Escreva aqui seu nome" 
+              />
+              <Button type="submit" disabled={!name}>
+                Jogar {name && 'como ' + name}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
+
         <Widget>
           <Widget.Content>
             <h1>CSS is Awesome!</h1>
@@ -47,7 +64,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/lfvperes"/>
+      <GitHubCorner projectUrl="https://github.com/lfvperes" />
     </QuizBackground>
   );
 }
